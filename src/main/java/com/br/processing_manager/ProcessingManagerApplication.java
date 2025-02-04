c75d5b9532f6591dc2cc97140e1871b2d9c31af0
Danilo Ferreira <danilo.ferreira@matera.com>
2025-02-04 17:04:35 -0300

    Estrutura inicial

---
+++ src/main/java/com/br/processing_manager/ProcessingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.br.processing_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessingManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/processing_manager/ProcessingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.processing_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
